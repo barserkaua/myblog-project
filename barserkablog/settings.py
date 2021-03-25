2a315191b007729dd98dd059fb655a882e6bdd43
Василь Перегинец <barserkaua@gmail.com>
2021-03-25 22:32:16 +0200

    Add mini home page

--- barserkablog/settings.py
+++ barserkablog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=60)
+    post_date = models.DateTimeField()
+    post_text = models.TextField(max_length=300)
+    post_image = models.ImageField(upload_to='event_images/')
