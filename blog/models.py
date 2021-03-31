8665c7473e32414030d1fe2fca532117992ff2b1
Василь Перегинец <barserkaua@gmail.com>
2021-03-31 22:50:01 +0300

    upgreat blog page

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,8 @@ from django.db import models
 class Post(models.Model):
     post_title = models.CharField(max_length=60)
     post_date = models.DateTimeField()
-    post_text = models.TextField(max_length=300)
+    post_text = models.TextField()
     post_image = models.ImageField(upload_to='event_images/')
+
+    def get_summary(self):
+        return self.post_text[:70]+'...'
