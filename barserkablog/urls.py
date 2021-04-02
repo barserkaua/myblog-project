22cd47eba73c21d5ddcce6d140b923d7ed4384ce
Василь Перегинец <barserkaua@gmail.com>
2021-04-03 00:43:43 +0300

    add contacts

--- barserkablog/urls.py
+++ barserkablog/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 import events.views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', events.views.home, name='home'),
     path('posts/', include('blog.urls')),
+    path('contacts/', views.contacts, name='contacts'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
